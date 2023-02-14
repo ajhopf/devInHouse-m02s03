e2436b8bc0ffc954bef6068a0f6f62e94abc224e
André Hopf <andre.hopf@gmail.com>
2023-02-14 10:28:27 -0300

    Exercicio 2

--- dietClinic/src/Clinica.java
+++ dietClinic/src/Clinica.java
@@ -1,7 +1,13 @@
 public class Clinica {
+    //exercicio 1
     String dono;
     String nomeDaClinica;
     String cnpj;
     String endereco;
     Integer codigo;
+
+    //exercicio 2
+    public Double calcularIMC (Double peso, Double altura) {
+        return (peso/altura * altura);
+    };
 }
